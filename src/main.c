002e7b3d37a1ab5d40c77dd91c44fe812825dff2
Miguel Sánchez de León Peque <peque@neosit.es>
2017-08-11 01:22:43 +0200

    Implement SysTick interruptions

--- src/main.c
+++ src/main.c
@@ -1,43 +1,76 @@
-#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/cm3/systick.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
 
 
-static void clock_setup(void)
+/**
+ * @brief Initial clock setup.
+ *
+ * Use the External High Speed clock (HSE), at 8 MHz, and set the SYSCLK
+ * at 72 MHz (the maximum allowed when using the external crystal/resonator).
+ * This output frequency is possible thanks to the Phase Locked Loop (PLL)
+ * multiplier.
+ *
+ * Enable required clocks for the GPIOs as well.
+ */
+static void setup_clock(void)
 {
-	/* Set STM32 to 72 MHz. */
-	rcc_clock_setup_in_hse_12mhz_out_72mhz();
-
-	/* Enable GPIOC, and AFIO clocks. */
+	rcc_clock_setup_in_hse_8mhz_out_72mhz();
 	rcc_periph_clock_enable(RCC_GPIOC);
-	rcc_periph_clock_enable(RCC_AFIO);
 }
 
 
-static void gpio_setup(void)
+/**
+ * @brief Initial GPIO configuration.
+ *
+ * Set GPIO modes and initial states.
+ */
+static void setup_gpio(void)
 {
-	/* Set GPIO13 (in GPIO port C) to 'output push-pull'. */
 	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_50_MHZ,
 		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
-
-	AFIO_MAPR |= AFIO_MAPR_SWJ_CFG_FULL_SWJ_NO_JNTRST;
-
-	/* GPIO initialization */
 	gpio_clear(GPIOC, GPIO13);
 }
 
 
+/**
+ * @brief Set SysTick interruptions frequency and enable SysTick counter.
+ *
+ * SYSCLK is at 72 MHz as well as the Advanced High-permormance Bus (AHB)
+ * because, by default, the AHB divider is set to 1, so the AHB clock has the
+ * same frequency as the SYSCLK.
+ *
+ * @see STM32F103x8/STM32F103xB data sheet and in particular the "Clock tree"
+ * figure.
+ */
+static void setup_systick(void)
+{
+	systick_set_frequency(1, 72000000);
+	systick_counter_enable();
+	systick_interrupt_enable();
+}
+
+
+/**
+ * @brief Handle the SysTick interruptions.
+ */
+void sys_tick_handler(void)
+{
+	gpio_toggle(GPIOC, GPIO13);
+}
+
+
+/**
+ * @brief Initial setup and infinite wait.
+ */
 int main(void)
 {
-	int i;
+	setup_clock();
+	setup_gpio();
+	setup_systick();
 
-	clock_setup();
-	gpio_setup();
-
-	while (1) {
-		gpio_toggle(GPIOC, GPIO13);
-		for (i = 0; i < 800000; i++)
-			__asm__("nop");
-	}
+	while(1);
 
 	return 0;
 }
