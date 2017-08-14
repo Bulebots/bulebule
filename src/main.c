5b21f259e03e85967915b52f148ea216c73b1bec
Miguel Sánchez de León Peque <peque@neosit.es>
2017-08-15 01:37:54 +0200

    Setup USART for bluetooth communication

--- src/main.c
+++ src/main.c
@@ -1,8 +1,15 @@
+#include <stdio.h>
+#include <errno.h>
+
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/cm3/systick.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/timer.h>
+#include <libopencm3/stm32/usart.h>
+
+
+int _write(int file, char *ptr, int len);
 
 
 /**
@@ -18,9 +25,13 @@
 static void setup_clock(void)
 {
 	rcc_clock_setup_in_hse_8mhz_out_72mhz();
+	rcc_periph_clock_enable(RCC_GPIOB);
 	rcc_periph_clock_enable(RCC_GPIOC);
 
-	/* Encoder timers */
+	/* Bluetooth */
+	rcc_periph_clock_enable(RCC_USART3);
+
+	/* Encoders */
 	rcc_periph_clock_enable(RCC_TIM1);
 	rcc_periph_clock_enable(RCC_TIM4);
 }
@@ -36,6 +47,25 @@ static void setup_gpio(void)
 	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_50_MHZ,
 		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
 	gpio_clear(GPIOC, GPIO13);
+}
+
+
+/**
+ * @brief Setup USART for bluetooth communication.
+ */
+static void setup_usart(void)
+{
+	gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
+		      GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART3_TX);
+
+	usart_set_baudrate(USART3, 921600);
+	usart_set_databits(USART3, 8);
+	usart_set_stopbits(USART3, USART_STOPBITS_1);
+	usart_set_parity(USART3, USART_PARITY_NONE);
+	usart_set_flow_control(USART3, USART_FLOWCONTROL_NONE);
+	usart_set_mode(USART3, USART_MODE_TX);
+
+	usart_enable(USART3);
 }
 
 
@@ -102,12 +132,31 @@ void sys_tick_handler(void)
 
 
 /**
+ * @brief Make `printf` send characters through the USART.
+ */
+int _write(int file, char *ptr, int len)
+{
+	int i;
+
+	if (file == 1) {
+		for (i = 0; i < len; i++)
+			usart_send_blocking(USART3, ptr[i]);
+		return i;
+	}
+
+	errno = EIO;
+	return -1;
+}
+
+
+/**
  * @brief Initial setup and infinite wait.
  */
 int main(void)
 {
 	setup_clock();
 	setup_gpio();
+	setup_usart();
 	setup_encoders();
 	setup_systick();
 
@@ -116,6 +165,7 @@ int main(void)
 		int right = timer_get_counter(TIM4);
 		for (int i = 0; i < 800000; i++)
 			__asm__("nop");
+		printf("Left: %d, Right: %d\n", left, right);
 	}
 
 	return 0;
