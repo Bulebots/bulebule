f43256a4452f793eb589c923ae834da3c48c8b87
Miguel Sánchez de León Peque <peque@neosit.es>
2017-09-18 19:33:28 +0200

    Integrate clang-format in Travis tests

---
+++ scripts/check_format.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+output=$(mktemp)
+errors=$(mktemp)
+find src/ -type f -regex ".*\.\(c\|h\)$" | while read fname; do
+	clang-format "$fname" > "$output"
+	git diff --color "$fname" "$output" >> "$errors"
+done
+rm -f "$output"
+if [ -s "$errors" ]; then
+	echo -e "\033[0;31mFORMATTING ERRORS!\033[0m"
+	cat "$errors"
+	rm -f "$errors"
+	exit 1
+fi
+rm -f "$errors"
