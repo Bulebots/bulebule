92661bb42d46be89ae7cfafdd6c2e42c7d091584
Miguel Sánchez de León Peque <peque@neosit.es>
2017-10-12 00:37:33 +0200

    Include system time in log messages

--- src/logging.h
+++ src/logging.h
@@ -7,14 +7,19 @@
 
 #include <libopencm3/stm32/usart.h>
 
+#include "clock.h"
+
 enum { LOG_LEVEL_INFO, LOG_LEVEL_WARNING, LOG_LEVEL_ERROR };
 static const char *const log_level_strings[] = {"INFO", "WARNING", "ERROR"};
 
 int _write(int file, char *ptr, int len);
 
 #define LOG_MESSAGE(level, format, arg...)                                     \
-	printf("%s,%s:%d," format "\n", log_level_strings[level], __FILE__,    \
-	       __LINE__, ##arg)
+	do {                                                                   \
+		uint32_t time = get_clock_ticks();                             \
+		printf("%ld,%s,%s:%d: " format "\n", time,                     \
+		       log_level_strings[level], __FILE__, __LINE__, ##arg);   \
+	} while (0)
 
 #define LOG_INFO(format, arg...) LOG_MESSAGE(LOG_LEVEL_INFO, format, ##arg)
 #define LOG_WARNING(format, arg...)                                            \
