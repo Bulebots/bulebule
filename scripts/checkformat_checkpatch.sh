dd6779d95b64d335fa6c30ef516f3770894dbd5e
Miguel Sánchez de León Peque <peque@neosit.es>
2017-11-08 23:32:03 +0100

    Ignore USE_NEGATIVE_ERRNO warnings in checkpatch

--- scripts/checkformat_checkpatch.sh
+++ scripts/checkformat_checkpatch.sh
@@ -4,4 +4,4 @@ wget --no-clobber --quiet $linux/plain/scripts/spelling.txt
 wget --no-clobber --quiet $linux/plain/scripts/checkpatch.pl
 wget --no-clobber --quiet $linux/plain/scripts/const_structs.checkpatch
 chmod +x checkpatch.pl
-./checkpatch.pl --terse --no-tree --max-line-length=80 --ignore ARRAY_SIZE,LEADING_SPACE,SPLIT_STRING,VOLATILE --file src/*.[ch]
+./checkpatch.pl --terse --no-tree --max-line-length=80 --ignore ARRAY_SIZE,LEADING_SPACE,SPLIT_STRING,USE_NEGATIVE_ERRNO,VOLATILE --file src/*.[ch]
