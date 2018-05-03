ff58648d36c463c2f705c2858e3cb19ebf9a55c4
ClaraC <clara.casas.castedo@gmail.com>
2018-05-03 21:26:33 +0200

    Gyroscope z configuration

--- src/mpu.c
+++ src/mpu.c
@@ -1,18 +1,25 @@
 #include "mpu.h"
 
+#define BYTE 8
 #define MPU_READ 0x80
 
+#define MPU_SMPLRT_DIV 25
+#define MPU_CONFIG 26
+#define MPU_GYRO_CONFIG 27
 #define MPU_SIGNAL_PATH_RESET 104
 #define MPU_PWR_MGMT_1 107
 #define MPU_USER_CTRL 106
 #define MPU_WHOAMI 117
+
+#define MPU_GYRO_ZOUT_H 71
+#define MPU_GYRO_ZOUT_L 72
 
 /**
  * @brief Read a mpu register.
  *
  * @param[in] address Register address.
  */
-uint8_t mpu_read_register(uint8_t address)
+static uint8_t mpu_read_register(uint8_t address)
 {
 	uint8_t reading;
 
@@ -32,7 +39,7 @@ uint8_t mpu_read_register(uint8_t address)
  * @param[in] address Register address.
  * @param[in] address Register value.
  */
-void mpu_write_register(uint8_t address, uint8_t value)
+static void mpu_write_register(uint8_t address, uint8_t value)
 {
 	gpio_clear(GPIOB, GPIO12);
 	spi_send(SPI2, address);
@@ -50,4 +57,38 @@ void mpu_write_register(uint8_t address, uint8_t value)
 uint8_t mpu_who_am_i(void)
 {
 	return mpu_read_register(MPU_WHOAMI);
+}
+
+/**
+ * @brief MPU-6500 board setup to configure the gyroscope z
+ *
+ * - Reset mpu restoring default settings
+ * - Reset signal path
+ * - Set SPI mode, reset I2C Slave module
+ * - Sample Rate Divider (dix) equal to 0 where: SampleRate = InternalSample /
+ *   (1 + div)
+ * - DLPF(Dual Low Pass Filter) configuration to 0 with 250 Hz of bandwidth
+ *   and InternalSample = 8 KHz
+ * - Gyroscope configuration to use DLPF and to select +-2000 dps and 16.4 LSB
+ *   º/s.
+ */
+void mpu_setup(void)
+{
+	mpu_write_register(MPU_PWR_MGMT_1, 0x80);
+	sleep_us(100000);
+	mpu_write_register(MPU_SIGNAL_PATH_RESET, 0x07);
+	sleep_us(100000);
+	mpu_write_register(MPU_USER_CTRL, 0x10);
+	mpu_write_register(MPU_SMPLRT_DIV, 0x00);
+	mpu_write_register(MPU_CONFIG, 0x00);
+	mpu_write_register(MPU_GYRO_CONFIG, 0x18);
+}
+
+/**
+ * @brief Get gyroscope z current raw value
+ */
+int16_t get_gyro_z_raw(void)
+{
+	return ((mpu_read_register(MPU_GYRO_ZOUT_H) << BYTE) |
+		mpu_read_register(MPU_GYRO_ZOUT_L));
 }
--- src/mpu.h
+++ src/mpu.h
@@ -3,13 +3,12 @@
 
 #include <stdint.h>
 
-#include <libopencm3/stm32/timer.h>
-
+#include "clock.h"
 #include "setup.h"
 
-uint8_t mpu_read_register(uint8_t address);
-void mpu_write_register(uint8_t address, uint8_t value);
-
 uint8_t mpu_who_am_i(void);
+
+void mpu_setup(void);
+int16_t get_gyro_z_raw(void);
 
 #endif /* __MPU_H */
