bc8dcde1f4285068a99121ea1557a598ff2eb326
ClaraC <clara.casas.castedo@gmail.com>
2018-10-25 22:18:54 +0200

    Include stm32f103x8 parameters, simplify EEPROM functions

--- src/eeprom.c
+++ src/eeprom.c
@@ -1,88 +1,58 @@
 #include "eeprom.h"
 
 /**
- * @brief ADC1 and ADC2 interruption routine.
+ * @brief Function to read eeprom data
  *
- * - Manage the ADC2 analog watchdog interruption flag.
- * - Send a message.
- * - Toggle a LED.
- * - Disable analog watchdog interruptions on injected channels.
+ * @param[in] num_elements Number of bytes to be read
+ * @param[out] output_data Pointer to a word to save the read data
  */
-void flash_read_data(uint32_t start_address, uint16_t num_elements, uint8_t *output_data)
- {
- 	uint16_t iter;
- 	uint32_t *memory_ptr= (uint32_t*)start_address;
+void read_eeprom_data(uint16_t num_elements, uint8_t *output_data)
+{
+	uint32_t eeprom_address = FLASH_EEPROM_ADDRESS;
+	uint16_t iter;
+	uint32_t *memory_ptr = (uint32_t *)eeprom_address;
 
- 	for(iter=0; iter<num_elements/4; iter++)
- 	{
- 		*(uint32_t*)output_data = *(memory_ptr + iter);
- 		output_data += 4;
- 	}
- }
-
-/**
- * @brief Function to get battery voltage.
- *
- * This function reads the voltage of the battery from the register configured
- * on the ADC2. The conversion is triggered on sensors state machine function
- * sm_emitter_adc.
- *
- * The value is converted from bits to voltage taking into account that the
- * battery voltage is read through a voltage divider.
- *
- *@return The battery voltage in volts.
- */
-uint32_t flash_program_data(uint32_t start_address, uint8_t *input_data, uint16_t num_elements)
- {
- 	uint16_t iter;
- 	uint32_t current_address = start_address;
- 	uint32_t page_address = start_address;
- 	uint32_t flash_status = 0;
-
- 	/*check if start_address is in proper range*/
- 	if((start_address - FLASH_BASE) >= (FLASH_PAGE_SIZE * (FLASH_PAGE_NUM_MAX+1)))
- 		return 1;
-
- 	/*calculate current page address*/
- 	if(start_address % FLASH_PAGE_SIZE)
- 		page_address -= (start_address % FLASH_PAGE_SIZE);
-
- 	flash_unlock();
-
- 	/*Erasing page*/
- 	flash_erase_page(page_address);
- 	flash_status = flash_get_status_flags();
- 	if(flash_status != FLASH_SR_EOP)
- 		return flash_status;
-
- 	/*programming flash memory*/
- 	for(iter=0; iter<num_elements; iter += 4)
- 	{
- 		/*programming word data*/
- 		flash_program_word(current_address+iter, *((uint32_t*)(input_data + iter)));
- 		flash_status = flash_get_status_flags();
- 		if(flash_status != FLASH_SR_EOP)
- 			return flash_status;
-
- 		/*verify if correct data is programmed*/
- 		if(*((uint32_t*)(current_address+iter)) != *((uint32_t*)(input_data + iter)))
- 			return FLASH_WRONG_DATA_WRITTEN;
- }
-
- return 0;
+	for (iter = 0; iter < num_elements / BYTES_PER_WORD; iter++) {
+		*(uint32_t *)output_data = *(memory_ptr + iter);
+		output_data += BYTES_PER_WORD;
+	}
 }
 
-void local_ltoa_hex(uint32_t value, uint8_t *out_string)
- {
- 	uint8_t iter;
+/**
+ * @brief Function to save data on eeprom
+ *
+ * - Unlock flash
+ * - Erase page
+ * - Program flash memory word by word (32-bits )and verify that it is written
+ *
+ * @param[in] input_data Pointer to the data to be flashed
+ * @param[in] num_elements Number of bytes to be flashed
+ * @return Flash state.
+ */
+uint32_t flash_eeprom_data(uint8_t *input_data, uint16_t num_elements)
+{
+	uint16_t iter;
+	uint32_t eeprom_address = FLASH_EEPROM_ADDRESS;
+	uint32_t flash_status = 0;
 
- 	/*end of string*/
- 	out_string += 8;
- 	*(out_string--) = 0;
+	flash_unlock();
 
- 	for(iter=0; iter<8; iter++)
- 	{
- 		*(out_string--) = (((value&0xf) > 0x9) ? (0x40 + ((value&0xf) - 0x9)) : (0x30 | (value&0xf)));
- 		value >>= 4;
- 	}
- }
+	flash_erase_page(eeprom_address);
+	flash_status = flash_get_status_flags();
+	if (flash_status != FLASH_SR_EOP)
+		return flash_status;
+
+	for (iter = 0; iter < num_elements; iter += BYTES_PER_WORD) {
+		flash_program_word(eeprom_address + iter,
+				   *((uint32_t *)(input_data + iter)));
+		flash_status = flash_get_status_flags();
+		if (flash_status != FLASH_SR_EOP)
+			return flash_status;
+
+		if (*((uint32_t *)(eeprom_address + iter)) !=
+		    *((uint32_t *)(input_data + iter)))
+			return FLASH_WRONG_DATA_WRITTEN;
+	}
+
+	return RESULT_OK;
+}
