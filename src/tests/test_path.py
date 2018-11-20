ba3a3ea32cd9b8e03d75dcaf1e5f53c6ba882134
Miguel Sánchez de León Peque <peque@neosit.es>
2018-11-20 19:23:58 +0100

    Refactor path tests

--- src/tests/test_path.py
+++ src/tests/test_path.py
@@ -15,78 +15,71 @@ def interface():
     yield from yield_cffi('./src/path')
 
 
+def smooth_path(interface, sharp, language):
+    """
+    Generate a smoothed path using the specified path language.
+    """
+    ffi, lib = interface
+    result = ffi.new('enum movement destination[30]')
+    language = getattr(lib, language)
+    lib.make_smooth_path(sharp.encode('ascii'), result, language)
+    result = stringify_enums(result, ffi, 'enum movement')
+    result = [x[5:] for x in result]
+    return result[:result.index('END')]
+
+
+@pytest.mark.parametrize('language', ['PATH_SAFE', 'PATH_DIAGONALS'])
+@pytest.mark.parametrize('sharp', [
+    '', 'F', 'FF', 'FLF', 'FRF', 'FLLF', 'FLRF', 'FRRF', 'FRLF', 'FRFLLF',
+])
+def test_path_smoother_start_stop(interface, sharp, language):
+    """
+    Start and stop should not really alter translations, no matter the
+    language.
+    """
+    reference = smooth_path(interface, sharp, language)
+    assert ['START'] + reference == \
+        smooth_path(interface, 'B' + sharp, language)
+    assert reference + ['STOP'] == \
+        smooth_path(interface, sharp + 'S', language)
+    assert ['START'] + reference + ['STOP'] == \
+        smooth_path(interface, 'B' + sharp + 'S', language)
+
+
 @pytest.mark.parametrize('language', ['PATH_SAFE', 'PATH_DIAGONALS'])
 @pytest.mark.parametrize('sharp,smooth', [
     ('F', ['FRONT']),
     ('FF', ['FRONT', 'FRONT']),
     ('F' * 12, ['FRONT'] * 12),
-    ('FS', ['FRONT', 'STOP']),
     ('FLF', ['FRONT', 'LEFT_90', 'FRONT']),
     ('FRF', ['FRONT', 'RIGHT_90', 'FRONT']),
-    ('FLFS', ['FRONT', 'LEFT_90', 'FRONT', 'STOP']),
-    ('FRFS', ['FRONT', 'RIGHT_90', 'FRONT', 'STOP']),
     ('FLLF', ['FRONT', 'LEFT_180', 'FRONT']),
     ('FRRF', ['FRONT', 'RIGHT_180', 'FRONT']),
-    ('FLLFS', ['FRONT', 'LEFT_180', 'FRONT', 'STOP']),
-    ('FRRFS', ['FRONT', 'RIGHT_180', 'FRONT', 'STOP']),
-    ('BS', ['START', 'STOP']),
-    ('BFS', ['START', 'FRONT', 'STOP']),
-    ('BFFS', ['START', 'FRONT', 'FRONT', 'STOP']),
-    ('BFLF', ['START', 'FRONT', 'LEFT_90', 'FRONT']),
-    ('BFRF', ['START', 'FRONT', 'RIGHT_90', 'FRONT']),
-    ('BFLLF', ['START', 'FRONT', 'LEFT_180', 'FRONT']),
-    ('BFRRF', ['START', 'FRONT', 'RIGHT_180', 'FRONT']),
-    ('BFRFLLFS',
-     ['START', 'FRONT', 'RIGHT_90', 'FRONT', 'LEFT_180', 'FRONT', 'STOP']),
+    ('FRFLLF', ['FRONT', 'RIGHT_90', 'FRONT', 'LEFT_180', 'FRONT']),
 ], ids=[
     'Move front once',
     'Move front twice',
     'Move front many',
-    'Move front once and stop',
     'Straight-to-straight 90-degrees left turn',
     'Straight-to-straight 90-degrees right turn',
-    'Straight-to-stop 90-degrees left turn',
-    'Straight-to-stop 90-degrees right turn',
     'Straight-to-straight 180-degrees left turn',
     'Straight-to-straight 180-degrees right turn',
-    'Straight-to-stop 180-degrees left turn',
-    'Straight-to-stop 180-degrees right turn',
-    'Start and stop',
-    'Start, front once and stop',
-    'Start, front twice and stop',
-    'Start-to-straight 90-degrees left turn',
-    'Start-to-straight 90-degrees right turn',
-    'Start-to-straight 180-degrees left turn',
-    'Start-to-straight 180-degrees right turn',
-    'Start-to-straight 90-degrees right turn, then 180-degrees left and stop',
+    'Straight-to-straight 90-degrees right turn, then 180-degrees left',
 ])
 def test_path_smoother_all(interface, sharp, smooth, language):
     """
     Test correct path smoothing for all path languages.
     """
-    ffi, lib = interface
-    result = ffi.new('enum movement destination[30]')
-    assert len(smooth) + 1 <= len(result)
-
-    language = getattr(lib, language)
-    lib.make_smooth_path(sharp.encode('ascii'), result, language)
-    result = stringify_enums(result, ffi, 'enum movement')
-    result = [x[5:] for x in result]
-    assert result[:len(smooth)] == smooth
-    assert result[len(smooth)] == 'END'
+    assert smooth == smooth_path(interface, sharp, language)
 
 
 @pytest.mark.parametrize('sharp,smooth', [
     ('FLRF', ['FRONT', 'LEFT', 'RIGHT', 'FRONT']),
     ('FRLF', ['FRONT', 'RIGHT', 'LEFT', 'FRONT']),
-    ('FLRFS', ['FRONT', 'LEFT', 'RIGHT', 'FRONT', 'STOP']),
-    ('FRLFS', ['FRONT', 'RIGHT', 'LEFT', 'FRONT', 'STOP']),
     ('FRLLF', ['FRONT', 'RIGHT', 'LEFT', 'LEFT', 'FRONT']),
 ], ids=[
     'Left-right zig-zag',
     'Right-left zig-zag',
-    'Left-right zig-zag with stop',
-    'Right-left zig-zag with stop',
     'Once we start zig-zag, we keep 90-degrees turns',
 ])
 def test_path_smoother_safe(interface, sharp, smooth):
@@ -94,14 +87,7 @@ def test_path_smoother_safe(interface, sharp, smooth):
     Test correct path smoothing with the safe language.
     """
     ffi, lib = interface
-    result = ffi.new('enum movement destination[30]')
-    assert len(smooth) + 1 <= len(result)
-
-    lib.make_smooth_path(sharp.encode('ascii'), result, lib.PATH_SAFE)
-    result = stringify_enums(result, ffi, 'enum movement')
-    result = [x[5:] for x in result]
-    assert result[:len(smooth)] == smooth
-    assert result[len(smooth)] == 'END'
+    assert smooth == smooth_path(interface, sharp, 'PATH_SAFE')
 
 
 @pytest.mark.parametrize('sharp,smooth', [
@@ -161,11 +147,4 @@ def test_path_smoother_diagonals(interface, sharp, smooth):
     Test correct path smoothing with the diagonals language.
     """
     ffi, lib = interface
-    result = ffi.new('enum movement destination[30]')
-    assert len(smooth) + 1 <= len(result)
-
-    lib.make_smooth_path(sharp.encode('ascii'), result, lib.PATH_DIAGONALS)
-    result = stringify_enums(result, ffi, 'enum movement')
-    result = [x[5:] for x in result]
-    assert result[:len(smooth)] == smooth
-    assert result[len(smooth)] == 'END'
+    assert smooth == smooth_path(interface, sharp, 'PATH_DIAGONALS')
