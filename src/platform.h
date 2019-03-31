e543fd41080b8959fc58da93dad83b747aa850ea
claraC <clara.casas.castedo@gmail.com>
2019-03-31 19:58:50 +0200

    Separate speaker platform functions

--- src/platform.h
+++ src/platform.h
@@ -1,8 +1,6 @@
 #ifndef __PLATFORM_H
 #define __PLATFORM_H
 
-#include <stdint.h>
-
 #include <libopencm3/cm3/dwt.h>
 #include <libopencm3/stm32/adc.h>
 #include <libopencm3/stm32/timer.h>
@@ -17,7 +15,5 @@ uint16_t read_encoder_right(void);
 float get_battery_voltage(void);
 uint8_t mpu_read_register(uint8_t address);
 void mpu_write_register(uint8_t address, uint8_t value);
-void speaker_on(float hz);
-void speaker_off(void);
 
 #endif /* __PLATFORM_H */
---
+++ src/speaker.h
@@ -0,0 +1,11 @@
+#ifndef __SPEAKER_H
+#define __SPEAKER_H
+
+#include <stdint.h>
+
+#include "setup.h"
+
+void speaker_on(float hz);
+void speaker_off(void);
+
+#endif /* __SPEAKER_H */
