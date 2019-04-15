f84c2ec1d8c0121f5d9cde383aefe2e2ed6e9a4d
claraC <clara.casas.castedo@gmail.com>
2019-04-15 20:54:57 +0200

    Group calibration variables on config

---
+++ src/config.c
@@ -0,0 +1,54 @@
+#include "config.h"
+
+static volatile float micrometers_per_count = MICROMETERS_PER_COUNT;
+static volatile float wheels_separation = WHEELS_SEPARATION;
+static volatile struct control_constants control = {
+    .kp_linear = KP_LINEAR,
+    .kd_linear = KD_LINEAR,
+    .kp_angular = KP_ANGULAR,
+    .kd_angular = KD_ANGULAR,
+    .kp_angular_front = KP_ANGULAR_FRONT,
+    .ki_angular_front = KI_ANGULAR_FRONT,
+    .kp_angular_side = KP_ANGULAR_SIDE,
+    .ki_angular_side = KI_ANGULAR_SIDE};
+static volatile float linear_speed_limit = LINEAR_SPEED_LIMIT;
+
+float get_micrometers_per_count(void)
+{
+	return micrometers_per_count;
+}
+
+void set_micrometers_per_count(float value)
+{
+	micrometers_per_count = value;
+}
+
+float get_wheels_separation(void)
+{
+	return wheels_separation;
+}
+
+void set_wheels_separation(float value)
+{
+	wheels_separation = value;
+}
+
+struct control_constants get_control_constants(void)
+{
+	return control;
+}
+
+void set_control_constants(struct control_constants value)
+{
+	control = value;
+}
+
+float get_linear_speed_limit(void)
+{
+	return linear_speed_limit;
+}
+
+void set_linear_speed_limit(float value)
+{
+	linear_speed_limit = value;
+}
--- src/config.h
+++ src/config.h
@@ -19,4 +19,37 @@
 #define SENSOR_FRONT_RIGHT_A 2.713
 #define SENSOR_FRONT_RIGHT_B 0.258
 
+/*Control constants*/
+#define KP_LINEAR 800.
+#define KD_LINEAR 1600.
+#define KP_ANGULAR 5.
+#define KD_ANGULAR 100.
+#define KP_ANGULAR_FRONT 50.
+#define KI_ANGULAR_FRONT 200.
+#define KP_ANGULAR_SIDE 200.
+#define KI_ANGULAR_SIDE 400.
+
+struct control_constants {
+	float kp_linear;
+	float kd_linear;
+	float kp_angular;
+	float kd_angular;
+	float kp_angular_front;
+	float ki_angular_front;
+	float kp_angular_side;
+	float ki_angular_side;
+};
+
+/*Speed constants*/
+#define LINEAR_SPEED_LIMIT 2.5
+
+float get_micrometers_per_count(void);
+void set_micrometers_per_count(float value);
+float get_wheels_separation(void);
+void set_wheels_separation(float value);
+struct control_constants get_control_constants(void);
+void set_control_constants(struct control_constants value);
+float get_linear_speed_limit(void);
+void set_linear_speed_limit(float value);
+
 #endif /* _CONFIG_H */
