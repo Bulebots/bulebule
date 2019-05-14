c51f80f0274801799d4dd2939a4846969989dc7b
claraC <clara.casas.castedo@gmail.com>
2019-05-14 23:44:00 +0200

    fixup! Cppcheck configuration

--- scripts/checkstatic_cppcheck.sh
+++ scripts/checkstatic_cppcheck.sh
@@ -2,10 +2,6 @@
 
 cd ..
 
-cppcheck --enable=all src/ -isrc/simulation -isrc/printf -I src/mmlib -I src --check-config --suppress=missingIncludeSystem --quiet --error-exitcode=1 
+cppcheck --enable=all src/ -isrc/simulation -isrc/printf -Isrc/mmlib -Isrc --check-config --suppress=missingIncludeSystem --quiet --error-exitcode=1
 
-cppcheck --enable=all src/ -isrc/simulation -isrc/printf -I src/mmlib -I src --template=gcc --suppress=unusedFunction  --suppress=missingInclude --quiet --error-exitcode=1
-
-
-
-
+cppcheck --enable=all src/ -isrc/simulation -isrc/printf -Isrc/mmlib -Isrc --template=gcc --suppress=unusedFunction  --suppress=missingInclude --quiet --error-exitcode=1
